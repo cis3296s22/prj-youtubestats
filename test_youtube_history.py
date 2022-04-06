dd1c91edd85222ab8d2178d57e052b6b9a15998f
tyler3490 <61132056+tyler3490@users.noreply.github.com>
2022-04-06 14:55:44 -0400

    Create test_youtube_history.py

---
+++ test_youtube_history.py
@@ -0,0 +1,45 @@
+from unittest import TestCase
+
+
+class TestAnalysis(TestCase):
+    def test_download_data(self):
+        self.fail()
+
+    def test_deprecated_download_data_via_youtube_dl_login(self):
+        self.fail()
+
+    def test_df_from_files(self):
+        self.fail()
+
+    def test_make_wordcloud(self):
+        self.fail()
+
+    def test_check_df(self):
+        self.fail()
+
+    def test_total_time(self):
+        self.fail()
+
+    def test_best_and_worst_videos(self):
+        self.fail()
+
+    def test_most_emojis_description(self):
+        self.fail()
+
+    def test_funniest_description(self):
+        self.fail()
+
+    def test_three_randoms(self):
+        self.fail()
+
+    def test_compute(self):
+        self.fail()
+
+    def test_graph(self):
+        self.fail()
+
+    def test_start_analysis(self):
+        self.fail()
+
+    def test_run(self):
+        self.fail()
